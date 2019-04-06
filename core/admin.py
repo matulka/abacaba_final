8c8fb66b3bbbe41d7675fb4b8a11154e473485ad
Dmitrij Borisov <parameciume@yandex.ru>
2019-04-06 13:10:03 +0300

    Added product and category adjusment

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
